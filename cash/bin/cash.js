905466d43d8b3f93a5a75d50876c6dc0c31d0d58
DaniCpn <46676778+DaniCpn@users.noreply.github.com>
2019-02-22 14:47:38 +0100

    new infos

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -4,43 +4,43 @@ const got = require('got');
 const money = require('money');
 const chalk = require('chalk');
 const ora = require('ora');
-const currencies = require('../lib/currencies.json');
+const currencies = require('../lib/currencies.json'); //Json file pour recup les infos des valeurs de change
 
-const {API} = require('./constants');
+const {API} = require('./constants'); //API pour changer les valeurs entre les changes
 
-const cash = async command => {
+const cash = async command => { //Asynch command pour attendre le résultats avant d'enchainer
 	const {amount} = command;
-	const from = command.from.toUpperCase();
+	const from = command.from.toUpperCase(); 
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
 	console.log();
-	const loading = ora({
+	const loading = ora({ //Affiche le taux 
 		text: 'Converting...',
 		color: 'green',
 		spinner: {
 			interval: 150,
 			frames: to
 		}
-	});
+	}); 
 
-	loading.start();
-
-	await got(API, {
+	loading.start();//Chargement de la data
+	
+	await got(API, { //Utilisation de l'API
 		json: true
 	}).then(response => {
 		money.base = response.body.base;
 		money.rates = response.body.rates;
 
-		to.forEach(item => {
-			if (currencies[item]) {
-				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
-			} else {
+		to.forEach(item => { //Boucle pour chaque change
+			if (currencies[item]) { //Si on a l'information sur la currency de l'item alors on load l'item
+				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`); //Convert amount from the base currency to the other currencies in the list
+			} else { //Sinon on affiche qu'on a pas trouvé la currency
 				loading.warn(`${chalk.yellow(`The "${item}" currency not found `)}`);
 			}
 		});
 
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
-	}).catch(error => {
+	}).catch(error => { //Prise en charge des erreurs
 		if (error.code === 'ENOTFOUND') {
 			loading.fail(chalk.red('Please check your internet connection!\n'));
 		} else {
@@ -50,4 +50,4 @@ const cash = async command => {
 	});
 };
 
-module.exports = cash;
+module.exports = cash; //Affiche module.exports
